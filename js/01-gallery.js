ce02ab854f31f335d1dc5c3b7ac2825bb5eadea0
Ruslan-Ts <r.ts@me.com>
2023-02-15 16:33:12 +0100

    +

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,49 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
+
+const markup = galleryItems.map(({ preview, original, description }) => {
+return `<div class="gallery__item">
+<a class="gallery__link" href="large-image.jpg">
+<img class="gallery__image"
+src="${preview}"
+data-source="${original}"
+alt="${description}"/>
+</a>
+</div>`}).join('');
+
+gallery.insertAdjacentHTML('afterbegin', markup);
+
+gallery.addEventListener('click', onImageClick);
+
+function onImageClick(evt) {
+    evt.preventDefault();
+    if (!evt.target.classList.contains('gallery__image')) {
+        return;
+    }
+
+    const instance = basicLightbox.create(`<img src="${evt.target.dataset.source}" width="800" height="auto"/>`,
+        {
+            onShow: () => document.addEventListener('keydown', onCloseModal),
+            onClose: () => document.removeEventListener('keydown', onCloseModal),
+        }
+    );
+    instance.show();
+
+    function onCloseModal(evt) {
+        if (evt.code === 'Escape') {
+            instance.close();
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
 
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
+
+gallery.addEventListener('click', onGalleryClick)
+
+const markup = galleryItems.map(({ preview, original, description }) => {
+return `<li><a class="gallery__item" href="large-image.jpg">
+<img class="gallery__image"
+src="${preview}"
+data-source="${original}"
+alt="${description}"/>
+</a></li>`}).join('');
+
+gallery.innerHTML = markup;
+
+
+
+function onGalleryClick(evt) {
+    evt.preventDefault();
+    const lightbox = new SimpleLightbox('.gallery__item', {
+        captionData: 'alt',
+        captionDelay: 250,
+    });
+
+}
 
 console.log(galleryItems);
